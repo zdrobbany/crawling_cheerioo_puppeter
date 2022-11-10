e64b5a226e1a3b8dee53443ac1975d1d40d19454
zdrobbany <zdrobbany@gmail.com>
2022-11-10 12:25:56 +0700

    change to crawl all endpoint

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ var crawlData = [];
 
 // Test Sample
 const sampleData = [
-    {"url": "xxx", "title": "asdqwe"},
-    {"url": "xyz", "title": "a"},
-    {"url": "yyy", "title": "b"},
+    { "url": "xxx", "title": "asdqwe" },
+    { "url": "xyz", "title": "a" },
+    { "url": "yyy", "title": "b" },
 ];
 
 const workSheetColumnName = [
@@ -22,21 +22,64 @@ const workSheetColumnName = [
 const workSheetName = "Data";
 const filePath = './data.xlsx';
 
-(async () => {
+const urlToCrawl = [
+    "https://www.tokopedia.com/hinoarmindo/etalase/sparepart",
+    "https://www.tokopedia.com/hinoarista/etalase/sparepart-hino-truck",
+    "https://www.tokopedia.com/hinohibaindo/etalase/spare-part",
+    "https://www.tokopedia.com/hinodayaguna/etalase/sparepart",
+    "https://www.tokopedia.com/hinoindosentosa/etalase/sparepart-dutro",
+    "https://www.tokopedia.com/mayagrahai/etalase/spare-part",
+    "https://www.tokopedia.com/dutahino/etalase/sparepart-fast-moving",
+    "https://www.tokopedia.com/hinompm/etalase/sparepart",
+    "https://www.tokopedia.com/hinocaturkokoh/etalase/suku-cadang-spareparts",
+    "https://www.tokopedia.com/hinocsbi/etalase/sparepart",
+    "https://www.tokopedia.com/hinovima/etalase/spareparts",
+    "https://www.tokopedia.com/hinopalembang/etalase/spare-parts",
+    "https://www.tokopedia.com/hinopersada/etalase/spare-part-hino",
+    "https://www.tokopedia.com/hino-lampung/etalase/sparepart",
+    "https://www.tokopedia.com/hinojayaindah/etalase/sparepart-fast-moving-hino",
+    "https://www.tokopedia.com/hinobengkulu/etalase/spare-part-hino",
+    "https://www.tokopedia.com/hinoriau/etalase/sparepart",
+    "https://www.tokopedia.com/hinotrans/etalase/sparepart",
+    "https://www.tokopedia.com/hinomanado/etalase/spareparts",
+    "https://www.tokopedia.com/hinopatrako/etalase/sparepart",
+    "https://www.tokopedia.com/hinokumala/etalase/spare-part",
+    "https://www.tokopedia.com/hinokumala/etalase/spare-part",
+    "https://www.tokopedia.com/hinomitra/etalase/sparepart-hino",
+    "https://www.tokopedia.com/hinoprima/etalase/spparepart-hino",
+    "https://www.tokopedia.com/antasenaagu/etalase/spare-part",
+    "https://www.tokopedia.com/ampsampit/product",
+    "https://www.tokopedia.com/hinokumala/etalase/spare-part",
+];
+
+const data = async () => {
+    var allData = [];
+    for (let i = 0; i < urlToCrawl.length; i++) {
+        var dataFromCrawl = await getData(urlToCrawl[i]);
+        allData = allData.concat(dataFromCrawl);
+        if (i === urlToCrawl.length-1) {
+            exportxls(allData, workSheetColumnName, workSheetName, filePath)
+        }
+    }
+}
+
+data()
+
+async function getData(url) {
     // Launch Browser
     const browser = await puppeteer.launch({
         headless: false,
         args: ['--no-sandbox']
     });
+
+
     // Open new Browser
     const page = await browser.newPage()
-    var checkerPage = true;
-    do {
-    // const url = 'https://www.tokopedia.com/hinopersada/product/page/' + pageNumber;
-    // const url = 'https://www.tokopedia.com/hinoarmindo/product/page/' + pageNumber;
-    const url = 'https://www.tokopedia.com/hinocaturkokoh/product/page/' + pageNumber;
-     // go to browser wait till load
-    page.goto(url, {waitUntil: "load"})
+    // var checkerPage = true;
+    // do {
+
+    // go to browser wait till load
+    page.goto(url, { waitUntil: "load" })
     function resolveAfter5Seconds() {
         return new Promise(resolve => {
             setTimeout(() => {
@@ -55,7 +98,6 @@ const filePath = './data.xlsx';
 
     // Start Evaluate PageData
     const pageData = await page.evaluate(() => {
-        // console.log('here: ', document.readyState === 'complete');
         return {
             html: document.documentElement.innerHTML
         }
@@ -64,35 +106,39 @@ const filePath = './data.xlsx';
     const $ = cheerio.load(pageData.html)
     var productElement = [];
     // .css-974pl -> product container display
-    await $('.css-974ipl > a').each((i,obj) => {
+    await $('.css-974ipl > a').each((i, obj) => {
         // const regex = /(\S){5}-(\S){5}/g // regex hinopersada
         // const regex = /\((\S){10}\)/g // regex hinoarmindo
-        const regex = /(\S){10}/g // regex hinocatur kokoh
+        const regex = /\(([^)]+)\)|\[([^)]+)\]/g // regex global
         var codeItem = obj.attribs.title.match(regex);
-
-        productElement[i]= {
+        if (typeof(codeItem) === "object" && codeItem !== null) {
+            codeItem = codeItem[0]
+        }
+        productElement[i] = {
             url: obj.attribs.href,
             title: obj.attribs.title,
             codeItem: codeItem
         }
         crawlData.push(productElement[i])
     });
-    // console.log(crawlData.length)
-    
-    if (productElement.length === 0) {
-        checkerPage = false
-        var jsonObject = JSON.stringify(crawlData);
-        // console.log(jsonObject);
-        exportxls(crawlData, workSheetColumnName, workSheetName, filePath)
-        await browser.close();
-    } else {
-        pageNumber++
-    }
-    }
-    while (checkerPage)
-})();
 
-async function autoScroll(page){
+    await browser.close();
+    return crawlData
+
+    /** Use if using many page **/
+    // if (productElement.length === 0) {
+    //     checkerPage = false
+    //     var jsonObject = JSON.stringify(crawlData);
+    //     // console.log(jsonObject);
+    //     exportxls(crawlData, workSheetColumnName, workSheetName, filePath)
+    // } else {
+    //     pageNumber++
+    // }
+    // }
+    // while (checkerPage)
+};
+
+async function autoScroll(page) {
     await page.evaluate(async () => {
         await new Promise((resolve) => {
             var totalHeight = 0;
@@ -102,7 +148,7 @@ async function autoScroll(page){
                 window.scrollBy(0, distance);
                 totalHeight += distance;
 
-                if(totalHeight >= scrollHeight - window.innerHeight){
+                if (totalHeight >= scrollHeight - window.innerHeight) {
                     clearInterval(timer);
                     resolve();
                 }
@@ -111,9 +157,11 @@ async function autoScroll(page){
     });
 }
 
-const exportxls =(obj, workSheetColumnName, workSheetName, filePath) => {
+const exportxls = (obj, workSheetColumnName, workSheetName, filePath) => {
+    // console.log('on export excel: ', obj)
     const data = obj.map(el => {
-        return [el.url , el.title, el.codeItem]
+        console.log([el.url, el.title, el.codeItem])
+        return [el.url, el.title, el.codeItem]
     });
     const workBook = XLSX.utils.book_new();
     const workSheetData = [
